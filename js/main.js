f02e16698ff34fbfbe7aaabd91d0895782d77fa9
Michael <michaelgandolfi7@gmail.com>
2022-10-16 14:05:56 -0500

    added local storage capability to store book titles

---
+++ js/main.js
@@ -0,0 +1,31 @@
+document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("#title").innerText = localStorage.getItem("Books") 
+
+
+
+
+// 9780140328721  
+
+function getFetch(){
+  const isbn = document.querySelector("input").value
+  const url = `https://openlibrary.org/isbn/${isbn}.json`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+
+        console.log(data.title)
+
+        if (!localStorage.getItem("Books")) {
+          localStorage.setItem("Books", data.title)
+        } else {
+          let books = localStorage.getItem("Books") + "; " + data.title
+          localStorage.setItem("Books", books)
+        }
+        document.querySelector("#title").innerText = localStorage.getItem("Books")       
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
